977ba8b52285aeda491bd92247bc30816d8eb6d6
Andrew Marderstein <andrew.marderstein@gmail.com>
2026-01-15 17:10:45 -0500

    update

--- scripts/helper_scripts/train_chrombpnet_models.from_tagaligns.jobscript.sh
+++ scripts/helper_scripts/train_chrombpnet_models.from_tagaligns.jobscript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-module load cuda/11.2
-module load cudnn/8.1
+# module load cuda/11.2
+# module load cudnn/8.1
 
 # https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
-eval "$(conda shell.bash hook)"
-conda activate chrombpnet
-module load system
-module load cairo
+# eval "$(conda shell.bash hook)"
+conda activate chrombpnet2
+# module load system
+# module load cairo
 
 ref_fasta=$1
 chrom_sizes=$2
